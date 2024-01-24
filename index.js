136e45395facfe25f932b8c19946f6a5b7a460e5
joy-mahmud <joymahmudiucse@gmail.com>
2024-01-24 11:51:02 +0600

    login registration problem fixed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var jwt = require('jsonwebtoken');
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
@@ -7,6 +8,22 @@ require('dotenv').config()
 //middleware
 app.use(cors())
 app.use(express.json())
+
+const verifyToken = (req, res, next) => {
+    if (!req.body.authorization) {
+        return res.status(401).send({ message: 'forbidden access' })
+    }
+
+    const token = req.body.authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'forbidden access' })
+        }
+
+        req.decoded = decoded
+        next()
+    })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xouwts.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,12 +41,44 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
-        const taskCollection = client.db('taskDb').collection('tasks')
-        const userCollection = client.db('taskDb').collection('users')
+        const taskCollection = client.db('houseHunter').collection('')
+        const userCollection = client.db('houseHunter').collection('users')
+
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'admin'
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+
+            }
+            next()
+        }
+        //verify dekiveryman middleware
+        const verifyDeliveryMan = async (req, res, next) => {
+            const email = req.decoded.email
+            const query = { email: email }
+            const user = await userCollection.findOne(query)
+            const isAdmin = user?.role === 'delivery_man'
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' })
+
+            }
+            next()
+        }
+        //jwt related api 
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
         //users related api
         app.post('/users', async (req, res) => {
             const user = req.body
-            console.log(user)
+            const email = user.email
+            // console.log(user)
             //checking user exist or not
             const query = { email: user.email }
             const existingUser = await userCollection.findOne(query)
@@ -37,10 +86,43 @@ async function run() {
                 return res.send({ message: 'user already exist', insertedId: null })
             }
             const result = await userCollection.insertOne(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({...result,token})
+        })
+        app.post('/login', async (req,res)=>{
+            const user = req.body
+            const email = user.email
+            const password = user.password
+            const query ={email:email}
+            const userInfo = await userCollection.findOne(query)
+            if(userInfo){
+        
+                // console.log("cp",password,userInfo.password)
+                if(password===userInfo.password){
+                    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+                    res.send({...userInfo,authorization:'ok',token})
+                }else{
+                    res.send("password doesn't match")
+                }
+            }
+            else{
+                res.send('register first')
+            }
+        })
+        app.post('/checkUser',verifyToken,async(req,res)=>{
+            const userEmail = req.decoded.email
+            console.log(userEmail)
+            const query = {email:userEmail}
+            const user = await userCollection.findOne(query)
+            const name =user.name
+            const email = user.email
+            const image =user.image
+            const role = user.role
+            const result ={name,email,image,role}
             res.send(result)
         })
 
-      
+
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
